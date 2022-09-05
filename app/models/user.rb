9bfb6c3a684cd50023a903187b83a3b59cd1499d
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-09-05 23:46:15 +0100

    Relationships between models and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    devise :database_authenticatable, :registerable,
+              :recoverable, :rememberable, :validatable
+    
+    has_many :operations
+    has_many :groups
+
+    validates :name, presence: true, length: { maximum: 100 }
 end
