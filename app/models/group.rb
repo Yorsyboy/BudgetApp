79785040bdc8d8f44c59c235e2c0ff18b80cb776
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-09-06 21:13:55 +0100

    Add devise views

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,6 @@ class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :operations
 
-  validates :name, presence: true, length: { maximum: 100 }
-  validates :icon, presence: true, length: { maximum: 100 }
+  validates :name, presence: true, length: { in: 1..100 }
+  validates :icon, presence: true, length: { in: 1..300 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   has_many :groups
   has_many :operations
 
-  validates :name, presence: true, length: { maximum: 100 }
+  validates :name, presence: true, length: { in: 1..100 }
 end
