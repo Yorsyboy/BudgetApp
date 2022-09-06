713098cca625a5025d9e7f70261e8473ba6ef12c
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-09-06 01:11:40 +0100

    Setup Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'home#index'
+
+  resources :groups, only: %i[index new create] do
+    resources :operations, only: %i[index]
+  end
+  resources :operations, only: %i[new create]
 end
