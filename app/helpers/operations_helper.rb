163e3647baebc79e038fe02b38fabbefd8e8a876
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-09-06 22:12:43 +0100

    Fix group error

--- app/helpers/operations_helper.rb
+++ app/helpers/operations_helper.rb
@@ -1,18 +1,17 @@
 module OperationsHelper
   def find_operations
-    operations = []
-    groups_operations = GroupsOperation.where(group_id: params[:group_id]).order(created_at: :desc)
-    groups_operations.each do |groups_operation|
-      operations << Operation.find(id: groups_operation.operation_id)
+    if params[:group_id]
+      Group.find(params[:group_id]).operations
+    else
+      Operation.all
     end
-    operations
   end
 
   def create_groups_operations(operation, array)
     i = 1
 
-    while i <= array.length
-      group = Group.find(id: array[i])
+    while i < array.length
+      group = Group.find_by(id: array[i])
       operation.groups << group
       i += 1
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_202515) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_204242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_202515) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id"
+  end
+
+  create_table "groups_operations", force: :cascade do |t|
+    t.bigint "group_id"
+    t.bigint "operation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "operations", force: :cascade do |t|
